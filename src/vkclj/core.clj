e308292ea6a240731184529b5c50e1cdeb1dcc29
tim <llStrainll@gmail.com>
2014-11-18 17:44:26 +0300

    now need pipe matching

--- src/vkclj/core.clj
+++ src/vkclj/core.clj
@@ -40,6 +40,39 @@
                                          ~'ans (~handler ~'ans)))
                     ~'answer {:error {:http_res ~'answer}})))))
 
+;
+; next 4 funcs / macro for uploading photos
+;
+
+(defmacro read_file [file_path]
+  `(try
+     (slurp ~file_path)
+     (catch Exception ~'e {:error (str "caught exception while reading file " ~file_path (.getMessage ~'e))})))
+
+; {:aid aid :access_token token}
+(vkreq __getupserv__ "photos.getUploadServer"
+       (fn [some_map]
+         (match (get-in some_map "upload_url")
+                (some_str :guard string?) some_str
+                some_else {:error {:from_vk some_map}})))
+
+(defn __upload_photo_process__ [upload_url, file_content]
+  (match @(http/post upload_url {:headers {"Content-Type" "multipart/form-data"} :multipart {:name "file1" :content file_content}})
+         {:status 200 :body body}
+         (match (decode body)
+                {:error error} {:error error}
+                some (match (get_response some)
+                            {:server server :photos_list photos_list :aid aid :hash hash} {:server server :photos_list photos_list :aid aid :hash hash}
+                            nil {:error some}))
+         answer {:error {:http_res answer}}))
+
+; {:aid aid :server server :photos_list photos_list :hash hash :access_token token}
+(vkreq __savephotos__ "photos.save"
+       (fn [lst]
+         (case (or (list? lst) (vector? lst))
+           true :ok
+           false {:error {:from_vk lst}})))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  public API functions  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -74,7 +107,14 @@
            false {:error {:from_vk some_int}})))
 
 ; {:gid gid :aid aid :photo_path photo_path}
-(defn upload_photo [])
+(defn upload_photo [{gid :gid aid :aid photo_path :photo_path token :access_token}]
+  (match (read_file photo_path)
+         {:error error} {:error error}
+         file_cont (match ())))
+;
+; TODO need write pipe matching macro
+;
+
 
 (def token "29888b63ab3627919a4e3f27eb6abf81fde006575dcd5d65b944626585503db1737288c8fbd1d67f56719")
 
